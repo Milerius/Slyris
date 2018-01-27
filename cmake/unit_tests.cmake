9431748e924ba9e6f295659c9a0d777106cff78a
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-01-27 21:04:15 +0100

    add new definition

--- cmake/unit_tests.cmake
+++ cmake/unit_tests.cmake
@@ -3,6 +3,9 @@ include(GoogleTest)
 macro(CREATE_UNIT_TEST EXECUTABLE_NAME CTEST_NAME PREFIX_NAME SOURCES)
     add_executable(${EXECUTABLE_NAME} ${SOURCES})
     target_link_libraries(${EXECUTABLE_NAME} GTest::GTest GTest::Main)
+    if (WIN32)
+        target_compile_definitions(${EXECUTABLE_NAME} PRIVATE _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
+    endif(WIN32)
     set_target_properties(${EXECUTABLE_NAME}
             PROPERTIES
             ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/lib"
